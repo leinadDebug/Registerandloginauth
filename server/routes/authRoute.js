f4312e2773dc298f07eccdd8015df5ca82b8c1e7
leinadDebug <akujuaobidaniel@gmail.com>
2024-03-19 19:52:44 +0100

    nothing modified

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const registerUser = require("../controllers/authControllers");
 
+
 router.post("/register", registerUser);
+
 
 module.exports = router;
