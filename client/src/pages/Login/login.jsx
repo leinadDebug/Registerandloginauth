e21d5e4830436d602014e249fe237b94483e5e42
leinadDebug <akujuaobidaniel@gmail.com>
2024-03-20 17:43:06 +0100

    made correction -- renaming variables

--- client/src/pages/Login/login.jsx
+++ client/src/pages/Login/login.jsx
@@ -3,6 +3,7 @@ import { Logo } from "./loginassests/constant";
 import { Form, Link } from "react-router-dom";
 import animImg from "./loginassests/img_4207.gif";
 import { BgAnimation } from "./loginassests/constant";
+import { useNavigate } from "react-router-dom";
 
 // importing reacticons
 import { AiOutlineSwapRight } from "react-icons/ai";
@@ -11,31 +12,47 @@ import { FaRegCircleUser } from "react-icons/fa6";
 import axios from "axios";
 
 export default function Login() {
+  const navigate = useNavigate();
   const [data, setdata] = useState([
     {
-      username: "",
+      email: "",
       password: "",
     },
   ]);
 
-  const handleClick = (e) => {
+  const handleClick = async (e) => {
     e.preventDefault();
+    const { email, password } = data;
 
-    let user = localStorage.getItem("username").replace(/"/g, "");
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8000/api/auth/login",
+        { email, password }
+      );
+      if (data.error) {
+        alert(data.error);
+      } else {
+        setdata({});
+        
+      }
+    } catch (err) {
+      console.log("Error");
+    }
+
+    let mail = localStorage.getItem("email").replace(/"/g, "");
     let pass = localStorage.getItem("password").replace(/"/g, "");
 
-    
-
-    if (data.username == null && data.password == null) {
+    if (data.email == null && data.password == null) {
       alert("Fill all required fields");
-    } else if (data.username !== user) {
-      alert("Please enter the correct Username");
+    } else if (data.email !== mail) {
+      alert("Please enter the correct email");
     } else if (data.password !== pass) {
       alert("password is incorrect");
     } else {
       alert("login is successful");
-      console.log(user + " must be qual to " + data.username);
-      console.log(pass + " must be qual to " + data.password);
+      console.log(mail + " must be equal to " + data.email);
+      console.log(pass + " must be equal to " + data.password);
+      navigate("/dashboard");
     }
   };
   return (
@@ -54,15 +71,15 @@ export default function Login() {
               If you already have an account log in
             </p>
             <section className="flex flex-col mt-10 m-3">
-              <label>Username</label>
+              <label>email</label>
               <div></div>
               <input
-                type="text"
-                name="username"
-                placeholder="username"
-                value={data.username}
+                type="email"
+                name="email"
+                placeholder="email"
+                value={data.email}
                 onChange={(e) => {
-                  setdata({ ...data, username: e.target.value });
+                  setdata({ ...data, email: e.target.value });
                 }}
                 className="px-4 py-2 my-2 rounded-full placeholder:text-[#767575] border-2 border-[#767575] hover:bg-slate-50"
               ></input>
