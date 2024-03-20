06063bc0184808fa57b8c254957987ed8d7c149f
leinadDebug <akujuaobidaniel@gmail.com>
2024-03-20 17:43:40 +0100

    --mod

--- client/src/pages/Dashboard/dashboard.jsx
+++ client/src/pages/Dashboard/dashboard.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../../context/useContext";
 
 export default function dashboard() {
+  const { user } = useContext(UserContext);
+
   return (
     <div>
       <Link to="/login">
         <button className=" bg-blue-500 p-2 rounded text-white">login</button>
       </Link>
-      
+
       <Link to={`/register`}>
         <button className="bg-green-500 p-2 ml-4 rounded text-white">
           Register
         </button>
       </Link>
+
+      
     </div>
   );
 }
