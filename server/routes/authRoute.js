f844344406aba4d76084919c69f72b84c4b41b01
leinadDebug <akujuaobidaniel@gmail.com>
2024-03-20 17:41:20 +0100

    --added the get('/profile')

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
 //this allows us to use the express router in the route folder
 const router = express.Router();
-const registerUser = require("../controllers/authControllers");
-
+const { registerUser, loginUser, getprofile } = require("../controllers/authControllers");
 
 router.post("/register", registerUser);
-
+router.post("/login", loginUser);
+router.get('/profile', getprofile)
 
 module.exports = router;
