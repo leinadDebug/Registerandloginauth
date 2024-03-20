3efaed8bd170cff5b96b5ef95970b0a1c3a717c4
leinadDebug <akujuaobidaniel@gmail.com>
2024-03-20 17:46:25 +0100

    required cookieparser

--- server/index.js
+++ server/index.js
@@ -4,16 +4,16 @@ const cors = require("cors");
 const main = require("./connect.cjs");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/authRoute.js");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/auth', authRoute)
-
+app.use("/api/auth", authRoute);
+app.use(express.urlencoded({ extended: false }));
 
 main();
-
 
 const port = 8000;
 app.listen(port, () => {
